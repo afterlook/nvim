46c008a2d0922a865f4e768d306e24372ee54e15
afterlook <29754364+afterlook@users.noreply.github.com>
2023-09-09 14:50:05 +0200

    Add rust and minor improvements across entire config
    Signed-off-by: afterlook <29754364+afterlook@users.noreply.github.com>

---
+++ lua/config/rust-tools.lua
@@ -0,0 +1,25 @@
+local rt = require("rust-tools")
+
+rt.setup({
+  server = {
+    on_attach = function(client, bufnr)
+      require("config/lsp").on_attach(client, bufnr)
+      local wk = require("which-key")
+      local default_options = { silent = true }
+      wk.register({
+        c = {
+          name = "Coding",
+          h = { "<cmd>lua require('rust-tools').hover_actions.hover_actions()<cr>", "Hoover docs", buffer = bufnr },
+          a = {
+            "<cmd>lua require('rust-tools').code_action_group.code_action_group()<cr>",
+            "Action",
+            buffer = bufnr,
+          },
+          r = { "<cmd>CargoReload<cr>", "Cargo reload", buffer = bufnr },
+        },
+      }, { prefix = "<leader>", mode = "n", default_options })
+    end,
+  },
+})
+
+require("rust-tools").inlay_hints.enable()
