855993ba8ab01a35397ae6e291ed0ab2050c1f40
afterlook <29754364+afterlook@users.noreply.github.com>
2023-09-18 22:34:04 +0200

    Autopairs update default config and add angle bracket
    Signed-off-by: afterlook <29754364+afterlook@users.noreply.github.com>

--- lua/config/nvim-autopairs.lua
+++ lua/config/nvim-autopairs.lua
@@ -1,52 +1,49 @@
 local remap = vim.api.nvim_set_keymap
-local npairs = require("nvim-autopairs")
-npairs.setup({})
+local npairs = require('nvim-autopairs')
+npairs.setup({ map_bs = false, map_cr = false })
+
+local Rule = require('nvim-autopairs.rule')
+local cond = require('nvim-autopairs.conds')
+
+npairs.add_rules({
+  Rule('<', '>'):with_pair(cond.before_regex('%a+')):with_move(function(opts)
+    return opts.char == '>'
+  end),
+})
+
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require('cmp')
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
 
 vim.g.coq_settings = { keymap = { recommended = false } }
 
-remap("i", "<esc>", [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true, noremap = true })
-remap("i", "<c-c>", [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true, noremap = true })
-remap("i", "<tab>", [[pumvisible() ? "<c-n>" : "<tab>"]], { expr = true, noremap = true })
-remap("i", "<s-tab>", [[pumvisible() ? "<c-p>" : "<bs>"]], { expr = true, noremap = true })
+-- these mappings are coq recommended mappings unrelated to nvim-autopairs
+remap('i', '<esc>', [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true, noremap = true })
+remap('i', '<c-c>', [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true, noremap = true })
+remap('i', '<tab>', [[pumvisible() ? "<c-n>" : "<tab>"]], { expr = true, noremap = true })
+remap('i', '<s-tab>', [[pumvisible() ? "<c-p>" : "<bs>"]], { expr = true, noremap = true })
 
 -- skip it, if you use another global object
 _G.MUtils = {}
 
-_G.PairIT = function()
-  local autopairs = {
-    ["("] = ")",
-    ["["] = "]",
-    ["{"] = "}",
-    ["<"] = ">",
-    [ [=[']=] ] = [=[']=],
-    [ [=["]=] ] = [=["]=],
-  }
-  local set_pairs = vim.keymap.set
-  for k, v in pairs(autopairs) do
-    set_pairs("i", k, function()
-      return k .. v .. "<Left>"
-    end, { expr = true, noremap = true })
-  end
-end
-
 MUtils.CR = function()
   if vim.fn.pumvisible() ~= 0 then
-    if vim.fn.complete_info({ "selected" }).selected ~= -1 then
-      return npairs.esc("<c-y>")
+    if vim.fn.complete_info({ 'selected' }).selected ~= -1 then
+      return npairs.esc('<c-y>')
     else
-      return npairs.esc("<c-e>") .. npairs.autopairs_cr()
+      return npairs.esc('<c-e>') .. npairs.autopairs_cr()
     end
   else
     return npairs.autopairs_cr()
   end
 end
-remap("i", "<cr>", "v:lua.MUtils.CR()", { expr = true, noremap = true })
+remap('i', '<cr>', 'v:lua.MUtils.CR()', { expr = true, noremap = true })
 
 MUtils.BS = function()
-  if vim.fn.pumvisible() ~= 0 and vim.fn.complete_info({ "mode" }).mode == "eval" then
-    return npairs.esc("<c-e>") .. npairs.autopairs_bs()
+  if vim.fn.pumvisible() ~= 0 and vim.fn.complete_info({ 'mode' }).mode == 'eval' then
+    return npairs.esc('<c-e>') .. npairs.autopairs_bs()
   else
     return npairs.autopairs_bs()
   end
 end
-remap("i", "<bs>", "v:lua.MUtils.BS()", { expr = true, noremap = true })
+remap('i', '<bs>', 'v:lua.MUtils.BS()', { expr = true, noremap = true })
