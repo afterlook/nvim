743a6356de87359607558ea8a16bc08c91969597
afterlook <29754364+afterlook@users.noreply.github.com>
2024-05-23 17:15:40 +0200

    Remove tree auto open
    Signed-off-by: afterlook <29754364+afterlook@users.noreply.github.com>

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -322,20 +322,3 @@ tree.setup({ -- BEGIN_DEFAULT_OPTS
     },
   },
 }) -- END_DEFAULT_OPTS
-
-local function open_nvim_tree(data)
-  -- buffer is a directory
-  local directory = vim.fn.isdirectory(data.file) == 1
-
-  if not directory then
-    return
-  end
-
-  -- change to the directory
-  vim.cmd.cd(data.file)
-
-  -- open the tree
-  require('nvim-tree.api').tree.open()
-end
-
-vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = open_nvim_tree })
