b5140278903441b7bdc93ce10e057ec8d93bebf0
afterlook <29754364+afterlook@users.noreply.github.com>
2024-05-23 16:42:15 +0200

    Make auto installation of lsp through mason
    Signed-off-by: afterlook <29754364+afterlook@users.noreply.github.com>

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -143,43 +143,50 @@ vim.g.coq_settings = {
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-for _, lsp in ipairs(servers) do
-  require('lspconfig')[lsp].setup({
-    on_attach = M.on_attach,
-    capabilities = capabilities,
-    diagnostics = {
-      globals = { 'vim' },
-    },
-  })
-end
-
--- Make runtime files discoverable to the server
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, 'lua/?.lua')
-table.insert(runtime_path, 'lua/?/init.lua')
-
-require('lspconfig').lua_ls.setup({
-  on_attach = M.on_attach,
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      defaultConfig = {
-        quote_style = 'none',
-      },
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT)
-        version = 'LuaJIT',
-        -- Setup your lua path
-        path = runtime_path,
-      },
+require('mason-lspconfig').setup_handlers({
+  -- The first entry (without a key) will be the default handler
+  -- and will be called for each installed server that doesn't have
+  -- a dedicated handler.
+  function(server_name) -- default handler (optional)
+    require('lspconfig')[server_name].setup({
+      on_attach = M.on_attach,
+      capabilities = capabilities,
       diagnostics = {
         globals = { 'vim' },
       },
-      workspace = { library = vim.api.nvim_get_runtime_file('', true) },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = { enable = false },
-    },
-  },
+    })
+  end,
+
+  ['lua_ls'] = function()
+    -- Make runtime files discoverable to the server
+    local runtime_path = vim.split(package.path, ';')
+    table.insert(runtime_path, 'lua/?.lua')
+    table.insert(runtime_path, 'lua/?/init.lua')
+
+    require('lspconfig').lua_ls.setup({
+      on_attach = M.on_attach,
+      capabilities = capabilities,
+      settings = {
+        Lua = {
+          defaultConfig = {
+            quote_style = 'none',
+          },
+          runtime = {
+            -- Tell the language server which version of Lua you're using (most likely LuaJIT)
+            version = 'LuaJIT',
+            -- Setup your lua path
+            path = runtime_path,
+          },
+          diagnostics = {
+            globals = { 'vim' },
+          },
+          workspace = { library = vim.api.nvim_get_runtime_file('', true) },
+          -- Do not send telemetry data containing a randomized but unique identifier
+          telemetry = { enable = false },
+        },
+      },
+    })
+  end,
 })
 
 return M
