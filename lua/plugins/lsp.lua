4707261d5df15a33aff0218003019a0c755c98e8
afterlook <29754364+afterlook@users.noreply.github.com>
2024-12-24 23:36:38 +0100

    Switch capabilities and boost suggestion snippets
    Signed-off-by: afterlook <29754364+afterlook@users.noreply.github.com>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,6 +16,7 @@ return {
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
+      'saghen/blink.cmp',
     },
     config = function()
       vim.g.coq_settings = {
@@ -27,8 +28,7 @@ return {
         },
       }
 
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      -- capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
       local on_attach = require('util/lsp').on_attach
 
       require('mason-lspconfig').setup_handlers({
