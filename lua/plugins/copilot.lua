0b7d5ab19e944ca0a9b92c5af89179fd3b8a03a1
afterlook <29754364+afterlook@users.noreply.github.com>
2025-03-15 22:51:05 +0100

    fix blink, treesitter and enable copilot auto
    Signed-off-by: afterlook <29754364+afterlook@users.noreply.github.com>

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -22,11 +22,11 @@ return {
         },
         suggestion = {
           enabled = true,
-          auto_trigger = false,
+          auto_trigger = true,
           hide_during_completion = true,
           debounce = 75,
           keymap = {
-            accept = '<M-l>',
+            accept = '<M-y>',
             accept_word = false,
             accept_line = false,
             next = '<M-]>',
