9b7371c64d19a43a2f8772cab71a3775ffd9d62f
afterlook <29754364+afterlook@users.noreply.github.com>
2025-04-25 11:04:11 +0200

    Updates, replacing telescope commands, replacing native lua nvim calls
    Signed-off-by: afterlook <29754364+afterlook@users.noreply.github.com>

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -6,5 +6,31 @@ return {
     opts = {
       'telescope',
     },
+    config = function()
+      local fzf = require('fzf-lua')
+      fzf.setup({
+        'telescope',
+        actions = {
+          files = {
+            true,
+            ['alt-i'] = false,
+            ['alt-h'] = false,
+            ['alt-f'] = false,
+            ['ctrl-i'] = { fn = fzf.actions.toggle_ignore, reuse = true, header = false },
+            ['ctrl-h'] = { fn = fzf.actions.toggle_hidden, reuse = true, header = false },
+            ['ctrl-f'] = { fn = fzf.actions.toggle_follow, reuse = true, header = false },
+          },
+          grep = {
+            true,
+            ['alt-i'] = false,
+            ['alt-h'] = false,
+            ['alt-f'] = false,
+            ['ctrl-i'] = { fn = fzf.actions.toggle_ignore, reuse = true, header = false },
+            ['ctrl-h'] = { fn = fzf.actions.toggle_hidden, reuse = true, header = false },
+            ['ctrl-f'] = { fn = fzf.actions.toggle_follow, reuse = true, header = false },
+          },
+        },
+      })
+    end,
   },
 }
