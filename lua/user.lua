5ce55720559643da068e7e427f39b6dbd1e78a88
afterlook <29754364+afterlook@users.noreply.github.com>
2025-05-23 09:26:15 +0200

    add clipboard keybind and move keybinds to new file
    Signed-off-by: afterlook <29754364+afterlook@users.noreply.github.com>

--- lua/user.lua
+++ lua/user.lua
@@ -48,20 +48,11 @@ vim.api.nvim_create_autocmd('ColorScheme', {
 
 set.colorcolumn = '100'
 
--- [[ Basic Keymaps ]]
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
--- Keymaps for better default experience
--- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
--- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
@@ -73,34 +64,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
-
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<C-n>', ':cprev<CR>', { silent = true })
-vim.keymap.set('n', '<C-m>', ':cnext<CR>', { silent = true })
--- vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
--- vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
-
--- Switching panes
---vim.keymap.set("n", "<c-h>", ":wincmd h<CR>", { silent = true })
---vim.keymap.set("n", "<c-l>", ":wincmd l<CR>", { silent = true })
---vim.keymap.set("n", "<c-j>", ":wincmd j<CR>", { silent = true })
---vim.keymap.set("n", "<c-k>", ":wincmd k<CR>", { silent = true })
-
--- gotodefs
-vim.api.nvim_set_keymap(
-  'n',
-  'gD',
-  '<cmd>lua vim.lsp.buf.declaration()<CR>',
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-  'n',
-  'gd',
-  '<cmd>lua vim.lsp.buf.definition()<CR>',
-  { noremap = true, silent = true }
-)
 
 -- lf plugin disable <leader>f
 vim.g.lf_map_keys = 0
@@ -141,8 +104,6 @@ WinCloseJmp = function()
 end
 
 vim.cmd([[ autocmd VimEnter,WinEnter * lua WinCloseJmp() ]])
-
-vim.keymap.set('n', '<c-q>', ':bd<CR>', { silent = true })
 
 -- disable mouse
 vim.opt.mouse = 'a'
